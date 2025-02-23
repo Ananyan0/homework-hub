6c8aa5e0c7a7f4080a1125d634083134fc12b165
Karen-xgit <ananyan2020>
2025-02-23 21:09:20 +0400

    Resolve Task3 sub Task2

---
+++ Task3/Task2/Program.cs
@@ -0,0 +1,45 @@
+﻿public class Student
+{
+    public string name;
+    public int age;
+    public float grade;
+
+    public Student(string name, int age, float grade)
+    {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    public void DisplayDetails()
+    {
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("Grade: " + grade);
+    }
+
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        int studentsCount = 2;
+        Student[] student1 = new Student[studentsCount];
+
+        for (int i = 0; i < studentsCount; i++)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            float grade = float.Parse(Console.ReadLine());
+
+            student1[i] = new Student(name, age, grade);
+        }
+
+        for (int i = 0; i < studentsCount; i++)
+        {
+            student1[i].DisplayDetails();
+        }
+        Console.ReadKey();
+    }
+}
