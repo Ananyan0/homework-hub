375a9735338c7ad196f57d4417aca2cb5846dcf4
Karen-xgit <ananyan2020>
2025-02-23 21:52:23 +0400

    Resolve Task5 sub Task2

---
+++ Task5/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿
+public class Program
+{
+ public void SwapNumbers(ref int a, ref int b){
+      int temp = a;
+      a = b;
+      b = temp;
+ }
+ 
+ public static void Main()
+ {
+      Program program = new Program();
+  
+      Console.WriteLine("Input first number:");
+      int a = int.Parse(Console.ReadLine());
+      Console.WriteLine("Input second number:");
+      int b = int.Parse(Console.ReadLine());
+  
+      program.SwapNumbers(ref a, ref b);
+  
+      Console.WriteLine($"number1 = {a} number2 = {b}");
+
+        Console.ReadKey();
+ }
+}
