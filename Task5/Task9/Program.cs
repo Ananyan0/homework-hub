d27effdc7090365ecd16407d97a46472a8ab9253
Karen-xgit <ananyan2020>
2025-02-23 22:04:12 +0400

    Resolve Task5 sub Task9

---
+++ Task5/Task9/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Program
+{
+    public void SecondToTime(out int hours, out int minutes, out int seconds, ref int total_sec)
+    {
+        hours = total_sec / 3600;
+        int temp = total_sec % 3600;
+        minutes = temp / 60;
+        seconds = total_sec % 60;
+    }
+
+    public static void Main()
+    {
+        Program program = new Program();
+
+        int total_sec = int.Parse(Console.ReadLine());
+        int hours, minutes, seconds;
+        if (total_sec > 0)
+        {
+            program.SecondToTime(out hours, out minutes, out seconds, ref total_sec);
+
+
+            Console.WriteLine($"{total_sec} is: hours: {hours} minutes: {minutes} seconds: {seconds}");
+
+            Console.ReadKey();
+        }
+    }
+}
