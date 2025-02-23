91b71b0687e8c4ca02f21b546ce0abcf90cdec69
Karen-xgit <ananyan2020>
2025-02-23 21:20:37 +0400

    Resolve Task3 sub Task5

---
+++ Task3/Task5/Program.cs
@@ -0,0 +1,50 @@
+﻿public class Shopping
+{
+    string name;
+    float price;
+    int quantity;
+
+    public Shopping(string name, float price, int quantity)
+    {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public float TotalPrice()
+    {
+        return price * quantity;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("How many products you will take?");
+        int product_count = int.Parse(Console.ReadLine());
+        Shopping[] shopping = new Shopping[product_count];
+        float cost = 0;
+
+        for (int i = 0; i < product_count; i++)
+        {
+            Console.WriteLine($"Enter the product's name:");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Enter the price for {name}:");
+            float price = float.Parse(Console.ReadLine());
+            Console.WriteLine($"Enter the quantity for {name}:");
+            int quantity = int.Parse(Console.ReadLine());
+
+            shopping[i] = new Shopping(name, price, quantity);
+        }
+
+        for (int i = 0; i < product_count; i++)
+        {
+            cost += shopping[i].TotalPrice();
+        }
+        if (product_count > 2) cost = cost - ((cost * 10) / 100);
+
+        Console.WriteLine(cost);
+        Console.ReadKey();
+    }
+}
