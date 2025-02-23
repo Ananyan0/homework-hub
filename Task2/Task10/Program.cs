5348c533516d798fe6ed51a44a01da90f15b3fe9
Mikael-xgit <michaelananyan1711>
2025-02-23 20:50:45 +0400

    Add  Task2 subtasks Task5, Task10

---
+++ Task2/Task10/Program.cs
@@ -0,0 +1,104 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        char[,] grid = new char[10, 10];
+        char target = 'X';
+        char player = 'P';
+        Random random = new Random();
+        int randomNumber1 = random.Next(0, 10);
+        int randomNumber2 = random.Next(0, 10);
+
+
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                grid[i, j] = ' ';
+            }
+        }
+        grid[randomNumber1, randomNumber2] = target;
+        int playerRow = 0;
+        int playerCol = 0;
+        grid[playerRow, playerCol] = player;
+        while (true)
+        {
+            Console.Clear();
+            Console.Write("  ");
+            for (int j = 0; j < 10; j++)
+            {
+                Console.Write(j + " ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(i + " ");
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(grid[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Input w, a, s or d");
+            char input = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            if (input == 'w' ||
+               input == 'a' ||
+               input == 's' ||
+               input == 'd')
+            {
+                switch (input)
+                {
+                    case 's':
+                        if (playerRow < 9)
+                        {
+                            grid[playerRow, playerCol] = ' ';
+                            playerRow++;
+                        }
+                        else Console.WriteLine("NO");
+                        break;
+                    case 'd':
+                        if (playerCol < 9)
+                        {
+                            grid[playerRow, playerCol] = ' ';
+                            playerCol++;
+                        }
+                        else Console.WriteLine("NO");
+                        break;
+                    case 'a':
+                        if (playerCol > 0)
+                        {
+                            grid[playerRow, playerCol] = ' ';
+                            playerCol--;
+                        }
+                        else Console.WriteLine("NO");
+                        break;
+                    case 'w':
+                        if (playerRow > 0)
+                        {
+                            grid[playerRow, playerCol] = ' ';
+                            playerRow--;
+                        }
+                        else Console.WriteLine("NO");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wrong input");
+            }
+            grid[playerRow, playerCol] = player;
+
+            if (grid[randomNumber1, randomNumber2] == grid[playerRow, playerCol])
+            {
+                Console.Clear();
+                Console.WriteLine("You Win!!!!");
+                break;
+            }
+        }
+        Console.ReadKey();
+
+    }
+}
---
+++ Task2/Task5/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+
+        int num = int.Parse(Console.ReadLine());
+        int count = 0;
+
+        if (num <= 1)
+        {
+            Console.WriteLine("Input the number which is greater then 1");
+            return;
+        }
+
+        for (int i = 1; i <= num; i++)
+        {
+            if (num % i == 0) count++;
+        }
+        if (count != 2)
+        {
+            Console.WriteLine("The number is not prime;");
+        }
+        else
+        {
+            Console.WriteLine("The number is prime");
+        }
+        Console.ReadKey();
+    }
+}
