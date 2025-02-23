2aca0739846b1ea03dffd86aaee7d80ff7dcf59d
Karen-xgit <ananyan2020>
2025-02-23 21:57:01 +0400

    Resolve Task5 sub Task5

---
+++ Task5/Task5/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+    public void area_perimeter(ref float radius, out float area, out float perimeter)
+    {
+        const float p = 3.14f;
+        area = p * (radius * radius);
+        perimeter = 2 * p * radius;
+    }
+
+
+    public static void Main()
+    {
+        Program program = new Program();
+
+        float radius = int.Parse(Console.ReadLine());
+        float area, perimeter;
+
+        program.area_perimeter(ref radius, out area, out perimeter);
+        Console.WriteLine($"Area: {area} \nPerimeter: {perimeter}");
+
+        Console.ReadKey();
+    }
+}
