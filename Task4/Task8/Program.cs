71d004fb454fe6b5ec605b37f46460ab5a147781
Karen-xgit <ananyan2020>
2025-02-23 21:44:58 +0400

    Resolve Task4 sub Task8

---
+++ Task4/Task8/Program.cs
@@ -0,0 +1,36 @@
+﻿public class Product
+{
+    public string name;
+    public double price;
+    public int quantity;
+
+    public Product(string name, double price, int quantity)
+    {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public void DisplayProduct()
+    {
+        Console.WriteLine($"Product's name is: {name} \nprice: {price} \nquantity: {quantity}\n");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Product[] product = new Product[3];
+
+        product[0] = new Product("Banana", 750, 4);
+        product[1] = new Product("Apple", 300, 10);
+        product[2] = new Product("Orange", 1200, 7);
+
+        foreach (var collect in product)
+        {
+            collect.DisplayProduct();
+        }
+        Console.ReadKey();
+    }
+}
