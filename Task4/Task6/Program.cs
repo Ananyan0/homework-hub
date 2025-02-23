cb43494ce60d5ae01b08d7f360ffe839d72c4929
Karen-xgit <ananyan2020>
2025-02-23 21:41:21 +0400

    Resolve Task4 sub Task6

---
+++ Task4/Task6/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Movie
+{
+    private int rating;
+
+    public int Rating
+    {
+        get { return rating; }
+        set
+        {
+            if (value >= 1 && value <= 5)
+            {
+                rating = value;
+                Console.WriteLine(rating);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Movie movie = new Movie();
+
+        movie.Rating = int.Parse(Console.ReadLine());
+
+        Console.ReadKey();
+    }
+}
