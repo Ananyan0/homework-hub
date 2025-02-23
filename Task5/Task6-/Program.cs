4bc6369befd8ce83b865cbc848a39032c1a6995d
Karen-xgit <ananyan2020>
2025-02-23 21:59:37 +0400

    Resolve Task5 sub Task6

---
+++ Task5/Task6-/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Program
+{
+    public void SumNumbers(params int[] numbers)
+    {
+        int sum = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            sum += numbers[i];
+        }
+        Console.WriteLine(sum);
+    }
+
+
+    public static void Main()
+    {
+        Program program = new Program();
+        int[] arr = new int[5];
+
+        for (int i = 0; i < 5; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        program.SumNumbers(arr);
+
+        Console.ReadKey();
+    }
+}
