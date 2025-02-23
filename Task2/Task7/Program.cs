bc4bd294b6120d4cd90d4b2473035a54e773def4
Mikael-xgit <michaelananyan1711>
2025-02-23 20:36:57 +0400

    Task2 -> Task7

---
+++ Task2/Task7/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        for (int i = 1; i <= 7; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write(i);
+            }
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+
+    }
+}
