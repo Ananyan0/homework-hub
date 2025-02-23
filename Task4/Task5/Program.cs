d8f01af6197d7a04ac47f8e5a343e59f0cd97c3a
Karen-xgit <ananyan2020>
2025-02-23 21:38:00 +0400

    Resolve Task4 sub Task5

---
+++ Task4/Task5/Program.cs
@@ -0,0 +1,38 @@
+﻿
+public class SmartWatch
+{
+    public string ownerName;
+    public int stepCount;
+
+    public SmartWatch(string ownerName)
+    {
+        this.ownerName = ownerName;
+        this.stepCount = 0;
+    }
+
+    public void AddSteps(int steps)
+    {
+        stepCount += steps;
+
+    }
+
+    public void ShowSteps()
+    {
+        Console.WriteLine($"{ownerName} you have {stepCount} steps");
+    }
+
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        SmartWatch mySmart = new SmartWatch("Karen");
+
+        mySmart.AddSteps(int.Parse(Console.ReadLine()));
+        mySmart.AddSteps(500);
+
+        mySmart.ShowSteps();
+
+    }
+}
